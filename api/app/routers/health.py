5b27d91feef20ec9083a2f05113043a533d55056
Nick Krempel <krempel@gmail.com>
2023-02-20 12:36:25 +0100

    Base API

---
+++ api/app/routers/health.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Request, Header
+from datetime import datetime
+
+router = APIRouter()
+
+
+@router.get('')
+def health(request: Request, user_agent: str | None = Header(default=None)):
+    return {
+        "status": "online",
+        "server_time": datetime.now(),
+        "client_host": request.client.host,
+        "user_agent": user_agent
+    }
