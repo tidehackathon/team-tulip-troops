d8cb9bbaec247dc85091786d4aac999dd3a954cf
Nick Krempel <krempel@gmail.com>
2023-02-21 13:47:30 +0100

    Epic loader bar

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -35,8 +35,21 @@ export class HomeComponent {
   articalFormControl = new FormControl('', Validators.required);
   isLoading: boolean = false;
   loaderValue: number = 0;
-
   int = interval(100);
+
+  job1: boolean = true;
+  job2: boolean = false;
+  job3: boolean = false;
+  job4: boolean = false;
+  job5: boolean = false;
+  job6: boolean = false;
+
+  job1done: boolean = false;
+  job2done: boolean = false;
+  job3done: boolean = false;
+  job4done: boolean = false;
+  job5done: boolean = false;
+  job6done: boolean = false;
 
   constructor(
     private api: ApiService,
@@ -63,6 +76,22 @@ export class HomeComponent {
   startLoader(): void {
     const subscription = this.int.subscribe(() => {
       this.loaderValue = this.loaderValue + 0.1;
+      if (this.loaderValue > 20) {
+        this.job1done = true;
+        this.job2 = true;
+      }
+      if (this.loaderValue > 40) {
+        this.job2done = true;
+        this.job3 = true;
+      }
+      if (this.loaderValue > 60) {
+        this.job3done = true;
+        this.job4 = true;
+      }
+      if (this.loaderValue > 80) {
+        this.job4done = true;
+        this.job5 = true;
+      }
       if (this.loaderValue > 100) {
         this.resetLoader();
         subscription.unsubscribe();
