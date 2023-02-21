2238e8f008d2e8d96f2fbf9413da7a564bb9a9a3
Papagaai35 <djgommers@gmail.com>
2023-02-21 08:35:41 +0100

    Adds clen_data function and Notes from the Q&A

--- pipeline/src/sentiment.py
+++ pipeline/src/sentiment.py
@@ -1,13 +1,21 @@
 import json
 import transformers
+import unicodedata
 
 
 classifier = transformers.pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
 
+def clean_data(input_dict):
+    tweetcontent = unicodedata.normalize('NFKD', input_dict['renderedContent']).encode('ascii', 'ignore').decode()
+    input['cleanRenderedContent'] = tweetcontent.replace("\n\n","").replace("\n",". ")
+    return input
+
 def get_sentiment(input_jsonstr):
     global classifier
     input = json.loads(input_jsonstr)
-    emotion = classifier(input['content'])
+    if 'cleanRenderedContent' not in input:
+        input = clean_data(input)
+    emotion = classifier(input['cleanRenderedContent'])
     input['emotion'] = {e['label']: e['score'] for e in emotion[0]}
     return json.dumps(input)
 
