66cc930115205a398f1e164ae82c21f9e4d88016
Gerben Rook <gerbenrook@gmail.com>
2023-02-22 14:46:22 +0100

    Fix entities

--- pipeline/src/entities.py
+++ pipeline/src/entities.py
@@ -21,7 +21,9 @@ def get_entities(input):
             'end': int(e['end']),
         })
     input_dict['entities'] = entities
-    return json.loads(json.dumps(input_dict))
+    print(entities)
+    return entities
+    # return json.loads(json.dumps(input_dict))
 
 
 def clean_data(input_dict):
